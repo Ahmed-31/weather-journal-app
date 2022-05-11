e50fe1a3e6d8e7d230b3cc4980c06f1a9405157b
Ahmed-31 <77246783+Ahmed-31@users.noreply.github.com>
2022-05-11 15:07:27 +0200

    edit the api key to the new one

--- website/app.js
+++ website/app.js
@@ -5,7 +5,7 @@ let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
 const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
-const apiKey = ',&appid=1823ed9b05f407b753677fc7f5b417a4=metric';
+const apiKey = ',&appid=5aca0bbb23343940f01aad543cba2b1c&units=metric';
 const server = "http://127.0.0.1:5500";
 
 const postData = async(url = '', data = {}) => {
